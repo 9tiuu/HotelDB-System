b908d9e42ebf1642bb3e14fb58f3be6411d4804a
KouhTest <jeremycecchi11@gmail.com>
2024-07-23 15:35:05 -0400

    Fecha y hora en Auditoría
    Se agrego fecha y hora actual al realizar una acción correspondiente al CRUD + Advertencia de usuario existente al crear un nuevo usuario.

--- Team/Jeremy/js/CRUD/users.js
+++ Team/Jeremy/js/CRUD/users.js
@@ -76,6 +76,16 @@ const AddUser = () => {
 
             userID += 1;
             UserCreateModal.classList.remove('active');
+
+        } else {
+            const userWarning = document.querySelector('.USERNAME');
+            userWarning.classList.add('warning');
+            userWarning.innerHTML = 'La habitación ya existe';
+
+            setTimeout(() => {
+                userWarning.classList.remove('warning');
+                userWarning.innerHTML = 'Nombre de usuario';
+            }, 2000);
         };
     };
 };
@@ -214,33 +224,42 @@ DataTableUser();
 /////////////////// Auditoria ///////////////////
 
 const createUserRecord = (username) => {
+    var today = new Date();
+    var now = today.toLocaleString();
+
     audit.innerHTML += `
         <div class="data-user user-add-data">
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-fill-add adduser" viewBox="0 0 16 16">
                 <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m.5-5v1h1a.5.5 0 0 1 0 1h-1v1a.5.5 0 0 1-1 0v-1h-1a.5.5 0 0 1 0-1h1v-1a.5.5 0 0 1 1 0m-2-6a3 3 0 1 1-6 0 3 3 0 0 1 6 0"/>
                 <path d="M2 13c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4"/>
-            </svg> <p>Se ha creado un nuevo usuario: <b class="user-name-data">${username}</b></p> <p class="info-data">16/06/2024 14:02</p>
+            </svg> <p>Se ha creado un nuevo usuario: <b class="user-name-data">${username}</b></p> <p class="info-data">${now}</p>
         </div>
     `;
 };
 
 const deleteUserRecord = (username) => {
+    var today = new Date();
+    var now = today.toLocaleString();
+
     audit.innerHTML += `
         <div class="data-user user-delete-data">
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-fill-x deleteuser" viewBox="0 0 16 16">
                 <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4"/>
                 <path d="M12.5 16a3.5 3.5 0 1 0 0-7 3.5 3.5 0 0 0 0 7m-.646-4.854.646.647.646-.647a.5.5 0 0 1 .708.708l-.647.646.647.646a.5.5 0 0 1-.708.708l-.646-.647-.646.647a.5.5 0 0 1-.708-.708l.647-.646-.647-.646a.5.5 0 0 1 .708-.708"/>
-            </svg> <p>Se ha eliminado el usuario: <b class="user-name-data">${username}</b></p> <p class="info-data">16/06/2024 14:10</p>
+            </svg> <p>Se ha eliminado el usuario: <b class="user-name-data">${username}</b></p> <p class="info-data">${now}</p>
         </div>
     `;
 };
 
 const editUserRecord = (username) => {
+    var today = new Date();
+    var now = today.toLocaleString();
+
     audit.innerHTML += `
         <div class="data-user user-edit-data">
             <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-fill-gear edituser" viewBox="0 0 16 16">
                 <path d="M11 5a3 3 0 1 1-6 0 3 3 0 0 1 6 0m-9 8c0 1 1 1 1 1h5.256A4.5 4.5 0 0 1 8 12.5a4.5 4.5 0 0 1 1.544-3.393Q8.844 9.002 8 9c-5 0-6 3-6 4m9.886-3.54c.18-.613 1.048-.613 1.229 0l.043.148a.64.64 0 0 0 .921.382l.136-.074c.561-.306 1.175.308.87.869l-.075.136a.64.64 0 0 0 .382.92l.149.045c.612.18.612 1.048 0 1.229l-.15.043a.64.64 0 0 0-.38.921l.074.136c.305.561-.309 1.175-.87.87l-.136-.075a.64.64 0 0 0-.92.382l-.045.149c-.18.612-1.048.612-1.229 0l-.043-.15a.64.64 0 0 0-.921-.38l-.136.074c-.561.305-1.175-.309-.87-.87l.075-.136a.64.64 0 0 0-.382-.92l-.148-.045c-.613-.18-.613-1.048 0-1.229l.148-.043a.64.64 0 0 0 .382-.921l-.074-.136c-.306-.561.308-1.175.869-.87l.136.075a.64.64 0 0 0 .92-.382zM14 12.5a1.5 1.5 0 1 0-3 0 1.5 1.5 0 0 0 3 0"/>
-            </svg> <p>Se han modificado los datos del usuario: <b class="user-name-data">${username}</b></p> <p class="info-data">16/06/2024 14:10</p>
+            </svg> <p>Se han modificado los datos del usuario: <b class="user-name-data">${username}</b></p> <p class="info-data">${now}</p>
         </div>
     `;
 };
